232e06ddc48455e3df44c5fb5530e553d979ce74
Rik Giles <rik.giles@gmail.com>
2019-08-21 19:10:02 +0800

    Updates

---
+++ precache-manifest.8dc6160bb0ed7d271bacf0ad0e614c49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10629c2ec10ae469256923c317f6c6e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8d52e60b7a09078fdf6",
+    "url": "/static/css/main.4f833dce.chunk.css"
+  },
+  {
+    "revision": "e35339f1dda50a7084e0",
+    "url": "/static/js/2.401ac7cb.chunk.js"
+  },
+  {
+    "revision": "b8d52e60b7a09078fdf6",
+    "url": "/static/js/main.7f2cdd83.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a8be2774c500c569bba99aa4ded83614.js"
+  "/precache-manifest.8dc6160bb0ed7d271bacf0ad0e614c49.js"
 );
 
 self.addEventListener('message', (event) => {
